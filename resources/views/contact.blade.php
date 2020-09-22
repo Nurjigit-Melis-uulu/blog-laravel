c3661f3ce856464c8b82a1a0bd27dae512e0b39c
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2020-09-22 19:29:43 +0600

    added form to contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,4 +4,30 @@
 
 @section('content')
   <h1>Contact</h1>
+
+  <form action="{{ route('contact-form') }}" method="post">
+    @csrf
+
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" name="name" placeholder="inter name" id="name" class="form-control"> 
+    </div>
+    
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input type="text" name="email" placeholder="inter email" id="email" class="form-control"> 
+    </div>
+
+    <div class="form-group">
+      <label for="subject">Message title</label>
+      <input type="text" name="subject" placeholder="inter subject" class="form-control" id="subject"> 
+    </div>
+
+    <div class="form-group">
+      <label for="message">Message</label>
+      <textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="inter message"></textarea>
+    </div>
+
+    <button type="submit" class="btn btn-success">Submit</button>
+  </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
-Route::get('/contact', function () {
+Route::get('/feedback', function () {
     return view('contact');
-});
+})->name('contact');
+
+Route::post('/contact/submit', function () {
+    dd(Request::all());
+})->name('contact-form');
